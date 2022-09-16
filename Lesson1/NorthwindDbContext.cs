d6562b86ce4753a74f7748d2240a85e5ad75ee8e
Emre Kisaboyun <emreksbyn@gmail.com>
2022-09-16 09:40:43 +0300

    Add project files.

---
+++ Lesson1/NorthwindDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options): base(options)
+        {
+        }
+        public NorthwindDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Northwind; Integrated Security=True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public int? EmployeeId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lesson1;
+using System.Data.SqlClient;
+
+#region SQL Queries in Code Without ORM
+await using SqlConnection connection = new SqlConnection(
+    $"Server=(localdb)\\MSSQLLocalDB;Database=Northwind; Integrated Security=True");
+await connection.OpenAsync();
+
+SqlCommand command = new("Select * from Employees", connection);
+SqlDataReader dataReader = await command.ExecuteReaderAsync();
+while (await dataReader.ReadAsync())
+{
+    Console.WriteLine($"{dataReader["FirstName"]} {dataReader["LastName"]}");
+}
+await connection.CloseAsync();
+#endregion
+
+
+#region With ORM
+NorthwindDbContext context = new();
+List<Employee> employees = context.Employees.ToList();
+foreach (var employee in employees)
+{
+    Console.WriteLine(employee.FirstName);
+} 
+#endregion
