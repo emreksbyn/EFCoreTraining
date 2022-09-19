8e669647e9dfdb072b9a92b49e832af0c9b911ba
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-09-19 11:56:06 +0300

    Lesson9 added.

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("");
+
+
+//// If I want to migrate without Microsoft.EntityFrameworkCore.Tools
+//ECommerceDbContext context = new();
+//await context.Database.MigrateAsync();
+
+
+// 1- Generate DbContext class
+public class ECommerceDbContext : DbContext
+{
+    // 3- Add Entities in DbContext class with DbSet<T>
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+
+    // 4- Override the OnConfiguring method. Give the database connection string
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // 5- Download "Microsoft.EntityFrameworkCore.SqlServer" if I use MS Sql Server. Than give the database connection string.
+        optionsBuilder.UseSqlServer("Server=.; Database=ECommerceDb; Integrated Security=True");
+    }
+}
+
+
+
+// 2- Generate Entities
+// Entities
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+}
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("");
+
+ECommerceDbContext context = new();
+await context.Database.MigrateAsync();
+
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Provider
+
+        // ConnectionString        
+        optionsBuilder.UseSqlServer("Server=.; Database=ECommerceDb; Integrated Security=True");
+
+        // Lazy Loading ...
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; } // OR
+    //public int ID { get; set; }
+    //public int ProductId { get; set; }
+    //public int ProductID { get; set; }
+}
+
+#region OnConfiguring() Method
+// We use OnConfiguring method to manipulate the EfCore.Tool with
+// It is an override method --> protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#endregion
+
+#region Basic Entity Identification Rules
+// Every Entity must have Primary Key in EfCore. --> public int Id { get; set; }
+#endregion
+
+#region Defination of Table Name
+//  public DbSet<Product> Products { get; set; } --> Products is Table name in Database
+#endregion
