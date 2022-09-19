aff1df1128617c8ac195534f419f128530316241
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-09-19 12:10:08 +0300

    Projects's reference updated.

--- DatabaseFirst/Contexts/NorthwindContext.cs
+++ DatabaseFirst/Contexts/NorthwindContext.cs
@@ -24,7 +24,6 @@ namespace DatabaseFirst.Contexts
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=.; Database=Northwind; Integrated Security=True");
             }
         }
@@ -83,35 +82,7 @@ namespace DatabaseFirst.Contexts
 
             modelBuilder.Entity<Product>(entity =>
             {
-                entity.HasIndex(e => e.CategoryId, "CategoriesProducts");
 
-                entity.HasIndex(e => e.CategoryId, "CategoryID");
-
-                entity.HasIndex(e => e.ProductName, "ProductName");
-
-                entity.HasIndex(e => e.SupplierId, "SupplierID");
-
-                entity.HasIndex(e => e.SupplierId, "SuppliersProducts");
-
-                entity.Property(e => e.ProductId).HasColumnName("ProductID");
-
-                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
-
-                entity.Property(e => e.ProductName).HasMaxLength(40);
-
-                entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
-
-                entity.Property(e => e.ReorderLevel).HasDefaultValueSql("((0))");
-
-                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
-
-                entity.Property(e => e.UnitPrice)
-                    .HasColumnType("money")
-                    .HasDefaultValueSql("((0))");
-
-                entity.Property(e => e.UnitsInStock).HasDefaultValueSql("((0))");
-
-                entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("((0))");
             });
 
             OnModelCreatingPartial(modelBuilder);
--- PersistingTheData/Program.cs
+++ PersistingTheData/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine();
+
+#region Add Data
+
+#endregion
+
+#region context.AddAsync()
+
+#endregion
+
+#region context.DbSet.AddAsync()
+
+#endregion
+
+#region SaveChanges() 
+
+#endregion
+
+#region How Does EFCore Know When Data Needs To Be Added?
+
+#endregion
+
+#region Attention To When There Is More Than One Data
+
+#endregion
+
+#region Efficient use of SaveChanges()
+
+#endregion
+
+#region AddRange()
+
+#endregion
+
+public class ECommerceDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.; Database=ECommerceDb; Integrated Security=True");
+    }
+}
+
+
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public float Price { get; set; }
+}
